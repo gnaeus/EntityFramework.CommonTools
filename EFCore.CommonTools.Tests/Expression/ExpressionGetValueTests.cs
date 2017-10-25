38d4868df46341b77ff8efd94e1092721e97d11d
PanyushkinD <PanyushkinD@quantumart.ru>
2017-10-25 13:01:45 +0300

    Optimize ExpressionExtensions.GetValue

--- EFCore.CommonTools.Tests/Expression/ExpressionGetValueTests.cs
+++ EFCore.CommonTools.Tests/Expression/ExpressionGetValueTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq.Expressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 #if EF_CORE
 namespace EntityFrameworkCore.CommonTools.Tests
@@ -12,9 +13,9 @@ namespace EntityFramework.CommonTools.Tests
     public class ExpressionGetValueTests
     {
         [TestMethod]
-        public void ShouldHandleConstants()
+        public void ShouldAcceptConstant()
         {
-            Expression<Func<int, int>> expression = num => 123;
+            Expression<Func<int>> expression = () => 123;
 
             object value = expression.Body.GetValue();
 
@@ -22,11 +23,91 @@ namespace EntityFramework.CommonTools.Tests
         }
 
         [TestMethod]
-        public void ShouldHandleClosures()
+        public void ShouldAcceptClosure()
         {
             int expected = 123;
 
-            Expression<Func<int, int>> expression = num => expected;
+            Expression<Func<int>> expression = () => expected;
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(expected, value);
+        }
+
+        public int ClassField = 123;
+
+        [TestMethod]
+        public void ShouldAcceptClassField()
+        {
+            Expression<Func<int>> expression = () => ClassField;
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(ClassField, value);
+        }
+
+        public int ClassProperty { get; set; } = 456;
+
+        [TestMethod]
+        public void ShouldAcceptClassProperty()
+        {
+            Expression<Func<int>> expression = () => ClassProperty;
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(ClassProperty, value);
+        }
+
+        class TestObject
+        {
+            public int Field;
+            public int Property { get; set; }
+            public int? Nullable { get; set; }
+            public int this[int i, int j] => i * j;
+        }
+
+        [TestMethod]
+        public void ShouldAcceptObjectField()
+        {
+            var obj = new TestObject { Field = 123 };
+
+            Expression<Func<int>> expression = () => obj.Field;
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(obj.Field, value);
+        }
+
+        [TestMethod]
+        public void ShouldAcceptObjectProperty()
+        {
+            var obj = new TestObject { Property = 456 };
+
+            Expression<Func<int>> expression = () => obj.Property;
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(obj.Property, value);
+        }
+
+        [TestMethod]
+        public void ShouldAcceptNullableConversion()
+        {
+            var obj = new TestObject { Nullable = 456 };
+
+            Expression<Func<int?>> expression = () => obj.Nullable;
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(obj.Nullable, value);
+        }
+
+        [TestMethod]
+        public void ShouldAcceptObjectConversion()
+        {
+            var expected = new TestObject();
+
+            Expression<Func<object>> expression = () => expected;
 
             object value = expression.Body.GetValue();
 
@@ -34,11 +115,78 @@ namespace EntityFramework.CommonTools.Tests
         }
 
         [TestMethod]
-        public void ShouldHandleExpressions()
+        public void ShouldAcceptObjectIndexer()
+        {
+            var obj = new TestObject();
+            int i = 123;
+            int j = 456;
+
+            Expression<Func<int>> expression = () => obj[i, j];
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(i * j, value);
+        }
+
+        [TestMethod]
+        public void ShouldAcceptListIndexer()
+        {
+            IReadOnlyList<int> list = new List<int> { 1, 2, 3 };
+            int i = 1;
+
+            Expression<Func<int>> expression = () => list[i];
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(list[i], value);
+        }
+
+        [TestMethod]
+        public void ShouldAcceptArrayIndexer()
+        {
+            var arr = new[] { 1, 2, 3 };
+            int i = 1;
+
+            Expression<Func<int>> expression = () => arr[i];
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(arr[i], value);
+        }
+
+        [TestMethod]
+        public void ShouldAcceptArrayLength()
+        {
+            var arr = new[] { 1, 2, 3 };
+
+            Expression<Func<int>> expression = () => arr.Length;
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(arr.Length, value);
+        }
+
+        [TestMethod]
+        public void ShouldAcceptComplexExpressions()
+        {
+            var obj = new TestObject { Field = 123, Nullable = 123 };
+            var list = new List<int> { 1, 2, 3 };
+
+            int expected = obj[list[2], (int)obj.Nullable];
+
+            Expression<Func<object>> expression = () => obj[list[2], (int)obj.Nullable];
+
+            object value = expression.Body.GetValue();
+
+            Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod]
+        public void ShouldFallBackToExpressionCompile()
         {
             int expected = 123;
 
-            Expression<Func<int, int>> expression = num => expected * expected;
+            Expression<Func<int>> expression = () => expected * expected;
 
             object value = expression.Body.GetValue();
 
--- EFCore.CommonTools/Expression/ExpressionExtensions.cs
+++ EFCore.CommonTools/Expression/ExpressionExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq.Expressions;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 
 #if EF_CORE
 namespace EntityFrameworkCore.CommonTools
@@ -19,33 +18,73 @@ namespace System.Linq.CommonTools
         /// <exception cref="InvalidOperationException" />
         public static object GetValue(this Expression expression)
         {
-            if (expression == null)
-            {
-                return null;
-            }
-            if (expression.NodeType == ExpressionType.Constant)
-            {
-                return ((ConstantExpression)expression).Value;
-            }
-            if (expression.NodeType == ExpressionType.MemberAccess)
-            {
-                var member = (MemberExpression)expression;
+            if (expression == null) throw new ArgumentNullException(nameof(expression));
 
-                if (member.Expression.NodeType == ExpressionType.Constant)
-                {
-                    var instance = (ConstantExpression)member.Expression;
+            switch (expression.NodeType)
+            {
+                case ExpressionType.Constant:
+                    return ((ConstantExpression)expression).Value;
 
-                    if (instance.Type.GetTypeInfo().IsDefined(typeof(CompilerGeneratedAttribute)))
+                case ExpressionType.MemberAccess:
+                    var memberExpr = (MemberExpression)expression;
                     {
-                        return instance.Type
-                            .GetField(member.Member.Name)
-                            .GetValue(instance.Value);
+                        object instance = memberExpr.Expression.GetValue();
+                        switch (memberExpr.Member)
+                        {
+                            case FieldInfo field:
+                                return field.GetValue(instance);
+
+                            case PropertyInfo property:
+                                return property.GetValue(instance);
+                        }
                     }
-                }
+                    break;
+
+                case ExpressionType.Convert:
+                    var convertExpr = (UnaryExpression)expression;
+                    {
+                        if (convertExpr.Method == null)
+                        {
+                            Type type = Nullable.GetUnderlyingType(convertExpr.Type) ?? convertExpr.Type;
+                            object value = convertExpr.Operand.GetValue();
+                            return Convert.ChangeType(value, type);
+                        }
+                    }
+                    break;
+
+                case ExpressionType.ArrayIndex:
+                    var indexExpr = (BinaryExpression)expression;
+                    {
+                        var array = (Array)indexExpr.Left.GetValue();
+                        var index = (int)indexExpr.Right.GetValue();
+                        return array.GetValue(index);
+                    }
+
+                case ExpressionType.ArrayLength:
+                    var lengthExpr = (UnaryExpression)expression;
+                    {
+                        var array = (Array)lengthExpr.Operand.GetValue();
+                        return array.Length;
+                    }
+
+                case ExpressionType.Call:
+                    var callExpr = (MethodCallExpression)expression;
+                    {
+                        if (callExpr.Method.Name == "get_Item")
+                        {
+                            object instance = callExpr.Object.GetValue();
+                            object[] arguments = new object[callExpr.Arguments.Count];
+                            for (int i = 0; i < arguments.Length; i++)
+                            {
+                                arguments[i] = callExpr.Arguments[i].GetValue();
+                            }
+                            return callExpr.Method.Invoke(instance, arguments);
+                        }
+                    }
+                    break;
             }
 
             // we can't interpret the expression but we can compile and run it
-
             var objectMember = Expression.Convert(expression, typeof(object));
             var getterLambda = Expression.Lambda<Func<object>>(objectMember);
 
